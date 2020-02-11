088431de32a249228ce6544f0921d3e051baab3a
Jonatan De la fuente Carques <jonatan.delafuente@JUANXXIII.net>
2020-02-11 17:08:43 +0100

    comenzamos el proyectogit commit -m ! (eclipse manda)

--- proyectoEEDDvehiculos/src/modelo/Coche.java
+++ proyectoEEDDvehiculos/src/modelo/Coche.java
@@ -1,5 +1,53 @@
 package modelo;
 
 public class Coche {
-
+ protected String marca;
+ protected String modelo;
+ protected String color;
+ protected enum tipoComb{
+	 DIESEL,GASOLINA,ELECTRICO,HIBRIDO	
+ }
+ protected short cilindrada;
+ protected byte plazas;
+ protected enum ambiental{
+	 A,B,C,ECO,ZERO
+ }
+ public Coche() {
+	 marca="";
+	 modelo="";
+	 color="";
+	 cilindrada=0;
+	 plazas=0;
+ }
+public String getMarca() {
+	return marca;
+}
+public void setMarca(String marca) {
+	this.marca = marca;
+}
+public String getModelo() {
+	return modelo;
+}
+public void setModelo(String modelo) {
+	this.modelo = modelo;
+}
+public String getColor() {
+	return color;
+}
+public void setColor(String color) {
+	this.color = color;
+}
+public short getCilindrada() {
+	return cilindrada;
+}
+public void setCilindrada(short cilindrada) {
+	this.cilindrada = cilindrada;
+}
+public byte getPlazas() {
+	return plazas;
+}
+public void setPlazas(byte plazas) {
+	this.plazas = plazas;
+}
+ 
 }
